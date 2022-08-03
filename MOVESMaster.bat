14efa27f45172931c9b716c29902bbcfbb0446ac
Daniel Bizer-Cox <bizer-cox.daniel@epa.gov>
2022-08-03 10:40:11 -0400

    updates MOVESMaster.bat to archive moveslog.txt files instead of only keeping the most recent log (#52)
    MOVESMaster now renames old log files with the current timestamp and adds them to `moveslog.zip`. This prevents the accidental deletion of old log files.

--- MOVESMaster.bat
+++ MOVESMaster.bat
@@ -1,7 +1,25 @@
 @echo off
+call setenv.bat
+
+set HR=%time:~0,2%
+set HR=%Hr: =0% 
+set HR=%HR: =%
+set logfile=moveslog_archive_%date:~10,4%-%date:~4,2%-%date:~7,2%_%HR%%time:~3,2%.txt
 if exist sharedwork\*.* erase /Q /S /F sharedwork\*.*
 if exist WorkerFolder\*.* erase /Q /S /F WorkerFolder\*.*
-if exist moveslog_old.txt erase moveslog_old.txt
-if exist moveslog.txt rename moveslog.txt moveslog_old.txt
-call setenv.bat
+
+if exist moveslog.txt REN "moveslog.txt" "%logfile%"
+
+if exist %logfile% (
+	if not exist moveslog.zip ( 
+		jar cMf moveslog.zip "%logfile%" && del "%logfile%"
+	) else ( 
+		jar uMf moveslog.zip "%logfile%" && del "%logfile%"
+	)
+)
+
 ant rungui
+
+
+
+
